b78870e37f4223c369332a22d363973104cf2b1b
Ivan Pesin <ivan@localhost>
2017-07-10 17:44:19 -0400

    Version with channel to avoid large memory consumption

--- knapsack.go
+++ knapsack.go
@@ -44,23 +44,20 @@ func greedy(items []item, maxWeight float64, metric func(i, j int) bool) (r []it
 
 }
 
-func combinations(items []item) [][]item {
-	sort.Slice(items, func(i, j int) bool { return items[i].name < items[j].name })
+func combinations(items []item, ch chan []item) {
+	defer close(ch)
 
 	p := int(math.Pow(2., float64(len(items))))
 
-	r := make([][]item, p)
-
 	for i := 0; i < p; i++ {
-		r[i] = []item{}
+		set := []item{}
 		for j := 0; j < len(items); j++ {
 			if (i>>uint(j))&1 == 1 {
-				r[i] = append(r[i], items[j])
+				set = append(set, items[j])
 			}
 		}
+		ch <- set
 	}
-
-	return r
 }
 
 func getSackWeight(set []item) (r float64) {
@@ -77,10 +74,14 @@ func getSackValue(set []item) (r float64) {
 	return
 }
 
-func bestSolution(set [][]item, maxWeight float64) (float64, []item) {
+func bestSolution(items []item, maxWeight float64) (float64, []item) {
 	bestVal := 0.
 	bestSack := []item{}
-	for _, sack := range set {
+
+	ch := make(chan []item)
+	go combinations(items, ch)
+
+	for sack := range ch {
 		if getSackWeight(sack) <= maxWeight {
 			v := getSackValue(sack)
 			if v > bestVal {
@@ -124,8 +125,9 @@ func main() {
 		fmt.Printf("Total value: $%.2f\n", v)
 	}
 
+	sort.Slice(items, func(i, j int) bool { return items[i].name < items[j].name })
 	fmt.Printf("Optimal solution:\n")
-	v, sack := bestSolution(combinations(items), knapsackSize)
+	v, sack := bestSolution(items, knapsackSize)
 	for _, i := range sack {
 		fmt.Println("   ", i)
 	}
